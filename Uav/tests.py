37f301c192de349952eed3c6d20c29e9cfb1b4e3
Onur-Yılmaz <onuryilmaz@trair.com.tr>
2024-03-21 09:52:34 +0300

    Add tests for UAV and Rental models

--- Uav/tests.py
+++ Uav/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from .models import UAV
 
-# Create your tests here.
+class UAVTestCase(TestCase):
+    def setUp(self):
+        self.uav_data = {
+            'model_name': 'Test UAV',
+            'max_flight_time': 10.5,
+            'weight': 25.0,
+            'communication_tech': 'wifi',
+            'operational_altitude': 500,
+            'max_speed': 100,
+            'payload_capacity': 5.0,
+            'endurance': 8.0,
+            'control_system': 'autonomous',
+            'applications': 'Test applications',
+            'onboard_computers': 2,
+        }
+
+    def test_uav_creation(self):
+        uav = UAV.objects.create(**self.uav_data)
+
+        self.assertEqual(uav.model_name, self.uav_data['model_name'])
+        self.assertEqual(uav.max_flight_time, self.uav_data['max_flight_time'])
+        self.assertEqual(uav.weight, self.uav_data['weight'])
+        self.assertEqual(uav.communication_tech, self.uav_data['communication_tech'])
+        self.assertEqual(uav.operational_altitude, self.uav_data['operational_altitude'])
+        self.assertEqual(uav.max_speed, self.uav_data['max_speed'])
+        self.assertEqual(uav.payload_capacity, self.uav_data['payload_capacity'])
+        self.assertEqual(uav.endurance, self.uav_data['endurance'])
+        self.assertEqual(uav.control_system, self.uav_data['control_system'])
+        self.assertEqual(uav.applications, self.uav_data['applications'])
+        self.assertEqual(uav.onboard_computers, self.uav_data['onboard_computers'])
--- rental/tests.py
+++ rental/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import timedelta
+from .models import Rental
+from Uav.models import UAV
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+class RentalModelTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.uav = UAV.objects.create(model_name='Test UAV', max_flight_time=10.0, weight=20.0,
+        communication_tech='wifi', operational_altitude=500,
+         max_speed=100, payload_capacity=5.0, endurance=8.0,
+         control_system='autonomous', applications='Test applications',
+         onboard_computers=2)
+
+    def test_rental_creation(self):
+        start_date = timezone.now().date() + timedelta(days=1)
+        end_date = timezone.now().date() + timedelta(days=5)
+        rental = Rental.objects.create(uav=self.uav, user=self.user, start_date=start_date, end_date=end_date)
+
+        self.assertEqual(rental.uav, self.uav)
+        self.assertEqual(rental.user, self.user)
+        self.assertEqual(rental.start_date, start_date)
+        self.assertEqual(rental.end_date, end_date)
+
+    def test_active_rental_validation(self):
+        start_date = timezone.now().date() + timedelta(days=1)
+        end_date = timezone.now().date() + timedelta(days=5)
+        Rental.objects.create(uav=self.uav, user=self.user, start_date=start_date, end_date=end_date)
+
+        with self.assertRaises(ValidationError) as cm:
+            Rental.objects.create(uav=self.uav, user=self.user, start_date=start_date, end_date=end_date)
+        self.assertEqual(cm.exception.messages[0], f"{self.uav.model_name} şu anda başka bir kiralama işlemi nedeniyle mevcut değil.")
+
+    def test_past_start_date_validation(self):
+        start_date = timezone.now().date() - timedelta(days=1)
+        end_date = timezone.now().date() + timedelta(days=5)
+
+        with self.assertRaises(ValidationError) as cm:
+            Rental.objects.create(uav=self.uav, user=self.user, start_date=start_date, end_date=end_date)
+        self.assertEqual(cm.exception.messages[0], "Başlangıç tarihi günümüzden geri olamaz.")
+
+
+    def test_end_date_before_start_date_validation(self):
+        start_date = timezone.now().date() + timedelta(days=1)
+        end_date = start_date - timedelta(days=1)
+
+        with self.assertRaises(ValidationError) as cm:
+            Rental.objects.create(uav=self.uav, user=self.user, start_date=start_date, end_date=end_date)
+        self.assertEqual(cm.exception.messages[0], "Bitiş tarihi, başlangıç tarihinden önce olamaz.")
+
